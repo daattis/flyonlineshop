a8419e7b1900c013ac1b37a81e3e6606da94095d
daattis <anne.tenhonen@gmail.com>
2021-06-14 13:29:54 +0300

    Separated server/script files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get("/", function(req,res){
+
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000.");
+});
